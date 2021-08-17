915bc3dde3eea03335bdffe81166e8b040337eff
RiwsPy <thibault.debruyne@hotmail.fr>
2021-08-17 10:15:15 +0200

    little fix

--- PurBeurre/PurBeurre/settings.py
+++ PurBeurre/PurBeurre/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t*!rwp+hhb&q*sfb#+5#bsrgn9c5xde3pm9d)9zweg&)9@%31('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
