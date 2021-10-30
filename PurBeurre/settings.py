6d6c7fc23c29a989dcdc73c232e0eac0033e89f9
RiwsPy <thibault.debruyne@hotmail.fr>
2021-10-30 12:44:34 +0200

    sentry + raven re-added in settings

--- PurBeurre/settings.py
+++ PurBeurre/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 import dj_database_url
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+import raven
 
 # SECURITY WARNING: don't run with debug turned on in production!
 load_dotenv()
@@ -46,6 +49,7 @@ INSTALLED_APPS = [
 
     'django_extensions',
     'debug_toolbar',
+    'raven.contrib.django.raven_compat',
 
     'catalogue.apps.CatalogueConfig',
     'user.apps.UserConfig',
@@ -164,6 +168,51 @@ DATABASES['default'].update(db_from_env)
 
 LOGGING = {
     'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'INFO', # WARNING by default. Change this to capture more than warnings.
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'INFO', # To capture more than ERROR, change to WARNING, INFO, etc.
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
+
+"""
+LOGGING = {
+    'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
@@ -194,5 +243,28 @@ LOGGING = {
         }
     }
 }
+"""
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
+
+sentry_sdk.init(
+    dsn="https://d42e418337c0497ca27ee7012c862b71@o1055791.ingest.sentry.io/6041945",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+RAVEN_CONFIG = {
+    'dsn': 'https://somethingverylong@sentry.io/216272', # caution replace by your own!!
+    # If you are using git, you can also automatically configure the
+    # release based on the git info.
+    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+}
+
